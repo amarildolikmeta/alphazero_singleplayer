882fadd7dcfb9151fed41237f4e70e96ef2d0f1a
amarildolikmeta <amarildolikmeta@gmail.com>
2020-02-17 12:57:19 +0100

    added posterior sampling mcts and envs

---
+++ envs/__init__.py
@@ -0,0 +1,6 @@
+from envs.taxi import generate_taxi
+from envs.chain import generate_chain
+from envs.loop import generate_loop
+from envs.river_swim import generate_river
+from envs.six_arms import generate_arms
+from envs.three_arms import generate_arms as generate_three_arms
