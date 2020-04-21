248a3b16ac3993300a11255c6c4d185342cc81e9
d1ggs <diego.piccinotti@mail.polimi.it>
2020-04-21 16:50:19 +0200

    Refactor argument parsing

--- evaluate_pf_method.py
+++ evaluate_pf_method.py
@@ -8,12 +8,13 @@ from datetime import datetime
 from joblib import Parallel, delayed
 import numpy as np
 import pandas as pd
-import argparse
 import os
 import matplotlib.pyplot as plt
 from helpers import smooth, symmetric_remove
 import time
 from envs.blackjack_pi import BlackjackEnv
+
+from utils.parser_setup import setup_parser
 
 plt.style.use('ggplot')
 from agent import agent
@@ -29,44 +30,9 @@ colors = ['r', 'b', 'g', 'orange', 'c', 'k', 'purple', 'y']
 markers = ['o', 's', 'v', 'D', 'x', '*', '|', '+', '^', '2', '1', '3', '4']
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--game', default='Blackjack_pi-v0', help='Training environment')
-    parser.add_argument('--grid', type=str, default="grid.txt", help='TXT file specfying the game grid')
-    parser.add_argument('--n_ep', type=int, default=1000, help='Number of episodes')
-    parser.add_argument('--n_mcts', type=int, default=20, help='Number of MCTS traces per step')
-    parser.add_argument('--max_ep_len', type=int, default=50, help='Maximum number of steps per episode')
-    parser.add_argument('--lr', type=float, default=0.01, help='Learning rate')
-    parser.add_argument('--c', type=float, default=1.5, help='UCT constant')
-    parser.add_argument('--cdpw', type=float, default=1, help='DPW constant')
-    parser.add_argument('--temp', type=float, default=1.0,
-                        help='Temperature in normalization of counts to policy target')
-    parser.add_argument('--gamma', type=float, default=1.0, help='Discount parameter')
-    parser.add_argument('--alpha', type=float, default=0.6, help='progressive widening parameter')
-    parser.add_argument('--data_size', type=int, default=1000, help='Dataset size (FIFO)')
-    parser.add_argument('--batch_size', type=int, default=32, help='Minibatch size')
-    parser.add_argument('--n_experiments', type=int, default=10, help='Number of experiments')
-    parser.add_argument('--window', type=int, default=25, help='Smoothing window for visualization')
-    parser.add_argument('--stochastic', action='store_true')
-    parser.add_argument('--alpha_test', action='store_true')
-    parser.add_argument('--visualize', action='store_true')
-    parser.add_argument('--gpu', action='store_true')
-    parser.add_argument('--eval_freq', type=int, default=20, help='Evaluation_frequency')
-    parser.add_argument('--eval_episodes', type=int, default=50, help='Episodes of evaluation')
-    parser.add_argument('--delta_alpha', type=float, default=0.2, help='progressive widening parameter')
-    parser.add_argument('--min_alpha', type=float, default=0, help='progressive widening parameter')
-    parser.add_argument('--max_alpha', type=float, default=2, help='progressive widening parameter')
-    parser.add_argument('--n_hidden_layers', type=int, default=2, help='Number of hidden layers in NN')
-    parser.add_argument('--n_hidden_units', type=int, default=16, help='Number of units per hidden layers in NN')
-    parser.add_argument('--n_epochs', type=int, default=10, help='Number of epochs of training for the NN')
-    parser.add_argument('--parallel', action='store_true')
-    parser.add_argument('--use_sampler', action='store_true')
-    parser.add_argument('--n_workers', type=int, default=4, help='Number of parallel workers')
-    parser.add_argument('--mcts_only', action='store_true')
-    parser.add_argument('--show_plots', action='store_true')
-    parser.add_argument('--particles', type=int, default=0, help='Numbers of particles to approximate state distributions')
-    parser.add_argument('--budget', type=int, default=1000, help='Computational budget')
 
-    args = parser.parse_args()
+    args = setup_parser()
+
     start_time = time.time()
     time_str = str(start_time)
     out_dir = 'logs/' + args.game + '/' + time_str + '/'
@@ -79,7 +45,7 @@ if __name__ == '__main__':
                 entry_point='envs.blackjack_pi:BlackjackEnv',
             )
         except:
-            print()
+            print("Something wrong registering Blackjack environment")
 
     # Disable running on GPU if not specifically requested
     if not args.gpu:
--- hyperopt_taxi.py
+++ hyperopt_taxi.py
@@ -1,19 +1,16 @@
 import argparse
-import os
-
+import os, pickle
 import numpy as np
 import hyperopt as hp
+from functools import partial
 
 from agent import agent
-
-from functools import partial
+from utils.parser_setup import setup_parser
 
 parameter_space = {"temp": hp.hp.quniform('temp', 0.05, 0.5, 0.05),
                    # "lr": hp.hp.qloguniform('lr', np.log(0.0001), np.log(0.1)),
                    "c": hp.hp.quniform('c', 0, 2, 0.1),
                    "alpha": hp.hp.quniform('alpha', 0, 0.99, 0.01)}
-
-import pickle
 
 
 def objective(params, keywords):
@@ -39,43 +36,9 @@ def objective(params, keywords):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--game', default='Blackjack_pi-v0', help='Training environment')
-    parser.add_argument('--grid', type=str, default="grid.txt", help='TXT file specfying the game grid')
-    parser.add_argument('--n_ep', type=int, default=1000, help='Number of episodes')
-    parser.add_argument('--n_mcts', type=int, default=20, help='Number of MCTS traces per step')
-    parser.add_argument('--max_ep_len', type=int, default=50, help='Maximum number of steps per episode')
-    parser.add_argument('--lr', type=float, default=0.01
-                        , help='Learning rate')
-    parser.add_argument('--c', type=float, default=1.5, help='UCT constant')
-    parser.add_argument('--cdpw', type=float, default=1, help='DPW constant')
-    parser.add_argument('--temp', type=float, default=1.0,
-                        help='Temperature in normalization of counts to policy target')
-    parser.add_argument('--gamma', type=float, default=1.0, help='Discount parameter')
-    parser.add_argument('--alpha', type=float, default=0.6, help='progressive widening parameter')
-    parser.add_argument('--data_size', type=int, default=1000, help='Dataset size (FIFO)')
-    parser.add_argument('--batch_size', type=int, default=32, help='Minibatch size')
-    parser.add_argument('--n_experiments', type=int, default=10, help='Number of experiments')
-    parser.add_argument('--window', type=int, default=25, help='Smoothing window for visualization')
-    parser.add_argument('--stochastic', action='store_true')
-    parser.add_argument('--alpha_test', action='store_true')
-    parser.add_argument('--visualize', action='store_true')
-    parser.add_argument('--gpu', action='store_true')
-    parser.add_argument('--eval_freq', type=int, default=20, help='Evaluation_frequency')
-    parser.add_argument('--eval_episodes', type=int, default=50, help='Episodes of evaluation')
-    parser.add_argument('--delta_alpha', type=float, default=0.2, help='progressive widening parameter')
-    parser.add_argument('--min_alpha', type=float, default=0, help='progressive widening parameter')
-    parser.add_argument('--max_alpha', type=float, default=2, help='progressive widening parameter')
-    parser.add_argument('--n_hidden_layers', type=int, default=2, help='Number of hidden layers in NN')
-    parser.add_argument('--n_hidden_units', type=int, default=16, help='Number of units per hidden layers in NN')
-    parser.add_argument('--n_epochs', type=int, default=10, help='Number of epochs of training for the NN')
-    parser.add_argument('--parallel', action='store_true')
-    parser.add_argument('--mcts_only', action='store_true')
-    parser.add_argument('--show_plots', action='store_true')
-    parser.add_argument('--particles', type=int, default=0, help='Numbers of particles to approximate state distributions')
 
+    args = setup_parser()
 
-    args = parser.parse_args()
     out_dir = ""
     # if not os.path.exists(out_dir):
     #     os.makedirs(out_dir)
