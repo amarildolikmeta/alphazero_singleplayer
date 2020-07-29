d50d156ca61e7e3cf4b445dc657a9f42a0819f3b
DIego Piccinotti <diego.piccinotti@mail.polimi.it>
2020-07-29 15:19:27 +0200

    Fix KeySet class never equal

--- pure_mcts/keyset.py
+++ pure_mcts/keyset.py
@@ -13,13 +13,23 @@ class KeySet(object):
         else:
             for k in state.keys():
                 if type(self.state[k]) == np.ndarray:
-                    self.state[k] = self.state[k].tostring()
+                    self.state[k] = (self.state[k]).tostring()
                 elif type(self.state[k]) == list:
                     self.state[k] = str(self.state[k])
 
-
     def __hash__(self):
         return hash(tuple(sorted(self.state.items())))
+
+    def __eq__(self, other):
+        if not hasattr(other, 'state'):
+            return False
+
+        for k in self.state:
+            if not (k in other.state and other.state[k] == self.state[k]):
+                return False
+
+        return True
+
 
 if __name__ == '__main__':
     h = KeySet([1, 0.5, 2])
