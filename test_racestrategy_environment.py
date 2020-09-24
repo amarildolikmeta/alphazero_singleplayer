0dcdeef1b890a8e1064d7044b709a9e3647bb66e
Diego Piccinotti <elimdor@live.it>
2020-09-24 18:07:58 +0200

    Test update

--- test_racestrategy_environment.py
+++ test_racestrategy_environment.py
@@ -7,22 +7,21 @@ _ = mdp.reset()
 ret = 0
 lap = 1
 
-agent_queue = mdp.get_agents_standings()
 start = time.time()
 while True:
     agent = mdp.get_next_agent()
     #print("Lap {}, agent {}".format(lap, agent))
-    print()
     a = np.random.choice([0, 1, 2, 3])
     s, r, done, _ = mdp.partial_step(a, agent)
     #print("Reward:" + str(r))
-    mdp.set_signature(mdp.get_signature())
+    #mdp.set_signature(mdp.get_signature())
     ret += r
     if mdp.has_transitioned():
         lap += 1
         now = time.time()
         milliseconds = now-start
-        print(milliseconds)
+        print("----------------------------------")
+        print("full step time:", milliseconds)
         start = time.time()
     if done:
         print("Return:", ret)
