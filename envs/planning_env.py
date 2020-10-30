e8efc0ee60743f905084153376da8af59517efc1
Diego Piccinotti <diego.piccinotti@gmail.com>
2020-10-30 17:36:54 +0100

    Abstract class for planning envs

---
+++ envs/planning_env.py
@@ -0,0 +1,37 @@
+from gym import Env
+import numpy as np
+from gym.utils import seeding
+
+
+class PlanningEnv(Env):
+
+    def __init__(self):
+        self.np_random = None
+        super(PlanningEnv, self).__init__()
+        self.action_space = None
+
+    def get_available_actions(self, agent: int) -> list:
+        assert self.action_space is not None, "Action space has not been initialized"
+        return [i for i in range(self.action_space.n)]
+
+    def get_next_agent(self) -> int:
+        return 0
+
+    def step(self, action):
+        raise NotImplementedError("step method must be implemented by subclasses of PlanningEnv")
+
+    def reset(self):
+        raise NotImplementedError("reset method must be implemented by subclasses of PlanningEnv")
+
+    def enable_search_mode(self):
+        pass
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def render(self, mode='human'):
+        print("[WARNING] No render mode is implemented for this environment")
+
+    def save_results(self, timestamp):
+        print("[WARNING] No save method is implemented for this environment")
