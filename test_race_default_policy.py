df152b5776792d0942285906eb03a3058de38819
Diego Piccinotti <diego.piccinotti@gmail.com>
2020-11-05 22:10:59 +0100

    Minor update

--- test_race_default_policy.py
+++ test_race_default_policy.py
@@ -18,9 +18,9 @@ if __name__ == '__main__':
     today = datetime.now()
     timestamp = today.strftime('%Y-%m-%d_%H-%M')
 
-    env = RaceEnv(horizon=100, scale_reward=False)
+    env = RaceEnv(horizon=100, scale_reward=False, randomize_events=True)
     rews = []
-    for i in trange(100):
+    for i in trange(1):
         env.reset()
         lap = 1
 
@@ -31,36 +31,34 @@ if __name__ == '__main__':
             actions = env.get_available_actions(agent)
             prob = PROBS[len(actions)]
             action = np.random.choice(actions, p=prob)
+
+            # Catalunya 2017 true
+
             # if lap == 14:
             #     action = 2
             # elif lap == 37:
             #     action = 1
             # else:
             #     action = 0
+
+            # Australia 2017 true
+            # if lap == 23:
+            #     action = env.map_compound_to_action("A3")
+            # else:
+            #     action = 0
+
             s, r, done, _ = env.partial_step(action, agent)
+            env.reset_stochasticity()
             # print(env.used_compounds)
             # print(env._pit_counts)
             cumulative += r
             lap += 1
             if done:
                 rews.append(cumulative.tolist())
-        # env.save_results(timestamp + "_default100_lin")
+        env.save_results(timestamp + "_aus_test")
     # print(rews)
     print("Return:", np.mean(rews, axis=0))
     print("std: +-", np.std(rews, axis=0))
     rews = np.array(rews)
-    np.save("data/RaceStrategy-v2/default_policy/results_unnorm.npy", rews)
-    #
-    # rews_budget = np.load('./logs/RaceStrategy-v2/multiagent/2020-10-11_11-35/results.npy')
-    # print(rews_budget)
-    #
-    # fig = plt.figure()
-    # plt.title('Return distribution - Catalunya 2017')
-    # sns.distplot(rews[:, 0], label='HAM def', hist=False)
-    # sns.distplot(rews[:, 1], label='VET def', hist=False)
-    # sns.distplot(rews_budget[:, 0], label='HAM - OL 1000b', hist=False)
-    # sns.distplot(rews_budget[:, 1], label='VET - PF 1000b', hist=False)
-    # plt.savefig('./logs/RaceStrategy-v2/multiagent/return_comparison_1000b_1000r.png')
-    #
-    #
+    #np.save("data/RaceStrategy-v2/default_policy/results_unnorm_aus.npy", rews)
 
