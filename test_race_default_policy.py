27d82511de029a9a3d426db1fb704ee5a2350f34
Diego Piccinotti <diego.piccinotti@gmail.com>
2020-12-06 13:00:53 +0100

    Update Env tester

--- test_race_default_policy.py
+++ test_race_default_policy.py
@@ -19,20 +19,21 @@ if __name__ == '__main__':
     timestamp = today.strftime('%Y-%m-%d_%H-%M')
 
     env = RaceEnv(horizon=100, scale_reward=False, randomize_events=False, start_lap=8)
-    print("Default Strategies")
-    strategy = [[0, "A5", 2, 0.0], [22, "A3", 0, 0.0], [42, "A3", 0, 0.0]]
-    env.reset()
-    print(env.simulate_strategy(env._pars_in, 'VET', strategy)[40: 45])
-    strategy = [[0, "A5", 2, 0.0], [22, "A3", 0, 0.0], [42, "A4", 0, 0.0]]
-    env.reset()
-    print(env.simulate_strategy(env._pars_in, 'VET', strategy)[40: 45])
-    strategy = [[0, "A5", 2, 0.0], [22, "A3", 0, 0.0]]
-    env.reset()
-    print(env.simulate_strategy(env._pars_in, 'VET', strategy)[40: 45])
+
+    # print("Default Strategies")
+    # strategy = [[0, "A5", 2, 0.0], [22, "A3", 0, 0.0], [42, "A3", 0, 0.0]]
+    # env.reset()
+    # print(env.simulate_strategy(env._pars_in, 'VET', strategy)[40: 45])
+    # strategy = [[0, "A5", 2, 0.0], [22, "A3", 0, 0.0], [42, "A4", 0, 0.0]]
+    # env.reset()
+    # print(env.simulate_strategy(env._pars_in, 'VET', strategy)[40: 45])
+    # strategy = [[0, "A5", 2, 0.0], [22, "A3", 0, 0.0]]
+    # env.reset()
+    # print(env.simulate_strategy(env._pars_in, 'VET', strategy)[40: 45])
     rews = []
 
-    print(env.map_compound_to_action("A3"))
-    print(env._median_tyre_laps)
+    # print(env.map_compound_to_action("A3"))
+    # print(env._tyre_expected_duration)
 
     #for special_action in [env.map_compound_to_action("A3"), env.map_compound_to_action("A4"), 0]:
     for i in trange(200):
@@ -43,10 +44,12 @@ if __name__ == '__main__':
         cumulative = np.zeros(env.agents_number)
         while not done:
             agent = env.get_next_agent()
-            actions = env.get_default_strategy(agent)
-            # actions = env.get_available_actions(agent)
-            prob = PROBS[len(actions)]
-            action = np.random.choice(actions, p=prob)
+            # actions = env.get_default_strategy(agent)
+            # # actions = env.get_available_actions(agent)
+            # prob = PROBS[len(actions)]
+            # action = np.random.choice(actions, p=prob)
+            # if action > 0:
+            #     print(lap)
 
             # Catalunya 2017 true
 
@@ -63,10 +66,10 @@ if __name__ == '__main__':
             # else:
             #     action = 0
 
-            # if lap == 21 or lap == 28:
-            #     action = env.map_compound_to_action("A3")
-            # else:
-            #     action = 0
+            if lap == 23:
+                action = env.map_compound_to_action("A3")
+            else:
+                action = 0
 
             # if lap == 14:
             #     action = env.map_compound_to_action("A3")
@@ -79,6 +82,11 @@ if __name__ == '__main__':
             #     action = 0
 
             s, r, done, _ = env.partial_step(action, agent)
+            # TODO state compression
+            sig = env.get_signature()
+            env.set_signature(sig)
+
+            # TODO safety car randomization (enable randomize_events in constructor)
             env.reset_stochasticity()
             # print(env.used_compounds)
             # print(env._pit_counts)
