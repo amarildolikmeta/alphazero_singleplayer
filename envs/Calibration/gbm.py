84ebade68cd0cefae241e191e1411c1e76a02c71
Edoardo Vittori <edoardo.vittori@polimi.it>
2021-01-28 17:28:38 +0100

    added vasicek

--- envs/Calibration/gbm.py
+++ envs/Calibration/gbm.py
@@ -25,10 +25,10 @@ def GBM_calibration_ll(params, log):
     mll = -LogLikelihood
     return mll
 
-def GBM_simulation(data, params):
+def GBM_simulation(data, params, N_Sim):
     m_hat = params[0]
     v_hat = params[1]
-    for num_plots in range(0,10):
+    for num_plots in range(0,N_Sim):
         log_step = [np.log(data['Price'].values[0])]
         for i in range(0,len(data)):
             log_step += [m_hat + np.sqrt(v_hat) * np.random.normal(0,1)]
@@ -46,89 +46,110 @@ def GBM_simulation(data, params):
 
 def GARCH_calibration(params, returns):
 
-    mu=params[0]
-    omega=abs(params[1])
-    alpha=abs(params[2])
-    beta=abs(params[3])
-    gamma=params[4]
+    mu = params[0]
+    omega = params[1]
+    alpha = params[2]
+    beta = params[3]
+    gamma = params[4]
     denum = 1-alpha-beta*(1+np.square(gamma))
     assert denum > 0, 'variance stationarity constraint breached '
     var_t = omega/denum
     LogLikelihood = 0
     for time in range(0,len(returns)):
-        innov = np.square(returns[time] - mu)
-        var_t = omega+alpha*var_t+beta*np.square(innov-gamma*np.sqrt(var_t))
+        innov = returns[time] - mu
+        var_t = omega + var_t * (alpha + beta * np.square(innov - gamma))
+        # var_t = omega+alpha*var_t+beta*np.square(innov-gamma*np.sqrt(var_t))
         assert var_t >= 0, "negative variance error"
-        LogLikelihood += -np.log(var_t) - innov / var_t
+        LogLikelihood += -np.log(var_t) - np.square(innov) / var_t
     mll = -LogLikelihood
     return mll
 
-# def NL_constr(params):
-#     # mu = params[0]
-#     # omega = abs(params[1])
-#     alpha = abs(params[2])
-#     beta = abs(params[3])
-#     gamma = params[4]
-#     denum = 1 - alpha - beta * (1 + np.square(gamma))
-#     # print(denum, denum>0)
-#     return denum
+def NL_constr(params):
+    alpha = params[2]
+    beta = params[3]
+    gamma = params[4]
 
-# def GARCH_calibration(data):
-#     returns = price2ret(data)
-#     x0 = np.array([0.1, 1.0, 0.1, 0.1, 0.1])
-#     params = optimize.minimize(NG_JGBM_LL, x0 = x0, args=returns, method="Nelder-Mead")
-#     return mu, omega, alpha, beta, gamma
+    return 1 - alpha - beta * (1 + np.square(gamma))
 
-def NGARCH_simulation(N_Sim ,T,params,S0, data):
-    mu=params[0]
-    omega=params[1]
-    alpha=params[2]
-    beta=params[3]
-    gamma=params[4]
+def NGARCH_simulation(data, params, N_Sim):
+    T = len(data)
+    S0 = data['Price'].values[0]
+    mu = params[0]
+    omega = params[1]
+    alpha = params[2]
+    beta = params[3]
+    gamma = params[4]
     denum = 1-alpha-beta*(1+np.square(gamma))
-    assert denum > 0
+    assert denum > 0, 'variance stationarity constraint breached '
     for num in range(0,N_Sim):
+        var_t = omega / (1 - alpha - beta)
         S = [S0]
-        v = omega
         for i in range(1,T):
-            mean=mu-0.5*v
             rdn = np.random.normal(0,1)
-            S+=[S[i-1]*np.exp(mean+np.sqrt(v)*rdn)]
-            v=omega+alpha*v+beta*np.square(np.sqrt(v)*rdn-gamma*np.sqrt(v))
+            var_t = omega + var_t * (alpha + beta * np.square(rdn - gamma))
+            S += [S[i-1]*np.exp((mu-1/2*var_t)+np.sqrt(var_t)*rdn)]
+
+
         plt.plot(S, color='blue')
     plt.plot(data['Price'], color='red')
     plt.show()
-        
+
+def vasicek_calibation(returns):
+    n=len(returns)-1
+    b_num = n*np.dot(returns[1:],returns[:-1]) - sum(returns[1:])*sum(returns[:-1])
+    b_denum = n*sum(np.square(returns[:-1]))-np.square(sum(returns[:-1]))
+    b = b_num/b_denum
+    c = (sum(returns[1:]-b*returns[:-1]))/n
+    delta2 = 1/n*(sum(np.square(returns[1:]-b*returns[:-1] - c)))
+
+    print("b,c,delta", b,c,delta2)
+    return [b, c, delta2]
+
+def vasicek_simulation(data, params, N_sim):
+    b = params[0]
+    c = params[1]
+    delta = np.sqrt(params[2])
+
+    for num_plots in range(0,N_sim):
+        S = [data['Price'].values[0]]
+        for i in range(1,len(data)):
+            S += [c + S[i-1] * b + delta*np.random.normal(0,1)]
+        plt.plot(S, color='blue')
+
+    plt.plot(data['Price'], color='red')
+
+    plt.show()
+    return S
 
 if __name__ == '__main__':
     # Import data
     path = "/Users/edoardovittori/Code/alphazero_singleplayer/utils/SP.CSV"
     data = pd.read_csv(path)
+    returns = price2ret(data)
+    N_sim = 10
 
     # calibrate and simulate GBM data with exact log likelihood
-    # log_returns = np.log(data['Price'].values[1:]) - np.log(data['Price'].values[:-1])
-    # params1 = GBM_calibration(log_returns)
-    # print('parameters are', params1)
-    # GBM_simulation(data, params1)
-    #
-    # # calibrate and simulate GBM data with optimized log likelihood
-    # x0 = np.array([ 0.001, 0.0001])
-    # params2 = optimize.minimize(GBM_calibration_ll, x0=x0, args=log_returns, method='Nelder-Mead')
-    # print('parameters are', params2.x)
-    # GBM_simulation(data, params2.x)
+    log_returns = np.log(data['Price'].values[1:]) - np.log(data['Price'].values[:-1])
+    params1 = GBM_calibration(log_returns)
+    print('parameters are', params1)
+    GBM_simulation(data, params1, N_sim)
 
-    fun = lambda x: (x[0] - 1) ** 2 + (x[1] - 2.5) ** 2
-    cons = ({'type': 'ineq', 'fun': lambda x: x[0] - 2 * x[1] + 2},
-            {'type': 'ineq', 'fun': lambda x: -x[0] - 2 * x[1] + 6},
-            {'type': 'ineq', 'fun': lambda x: -x[0] + 2 * x[1] + 2})
-    bnds = ((0, None), (0, None))
-    res = optimize.minimize(fun, (2, 0), method='SLSQP', bounds=bnds,
-                   constraints=cons)
+    # calibrate and simulate GBM data with optimized log likelihood
+    x0 = np.array([ 0.001, 0.0001])
+    params2 = optimize.minimize(GBM_calibration_ll, x0=x0, args=log_returns, method='Nelder-Mead')
+    print('parameters are', params2.x)
+    GBM_simulation(data, params2.x, N_sim)
 
+    # calibrate and simulate GBM with stochastic volatility
     x0 = np.array([0.1, 1.0, 0.1, 0.1, 0.1])
-    returns = price2ret(data)
-    bnds = ((None, None), (0, None), (0, None), (0, None), (None, None))
-    cons = ({'type': 'ineq', 'fun': lambda x: 1 - x[2] - x[3]*(1+x[4]**2)})
-    params3 = optimize.minimize(GARCH_calibration, x0=x0, args=returns, method="SLSQP", bounds=bnds, constraints=cons)
+    # mu, omega, alpha, beta, gamma
+    bnds = ((None, None), (0, None), (0, None), (0, None), (0, None))
+    # cons = ({'type': 'ineq', 'fun': lambda x: 1 - x[2] - x[3]*(1+x[4]**2)})
+    cons = ({'type': 'ineq', 'fun': NL_constr})
+    params3 = optimize.minimize(GARCH_calibration, x0=x0, args=returns, method="SLSQP", constraints=cons, bounds = bnds)
     print('parameters are', params3.x)
-    NGARCH_simulation(10, len(data), params3.x, data['Price'].values[0], data)
+    NGARCH_simulation(data, params3.x, N_sim)
+
+    # calibrate and simulate Vasicek
+    params4 = vasicek_calibation(data['Price'].values)
+    vasicek_simulation(data, params4, N_sim)
